135699a209a32bd970c566b7504e1e0a5e85b0a6
Laura Gao <lauragao86868@gmail.com>
2021-05-15 15:38:30 -0400

    init api request works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import InputEdge from './components/InputEdge';
+import AddEdgeButton from './components/AddEdgeButton';
+import axios from 'axios';
+import { Input } from 'semantic-ui-react';
 
 function App() {
+  const [value, setValue] = useState("");
+  const [res, setRes] = useState("")
+
+  const handleClick =  () => {
+    setRes("Training... do not exit this page.");
+    onSubmit();
+  }
+
+  const onSubmit = async () => {
+    axios.post("http://localhost:5000/qaoa", {"edges": value})
+    .then((response) => {
+      console.log(response);
+      setRes(response.data.maxcut)
+    })
+    .catch((err) => {
+      console.log(err);
+      setRes("Error");
+    });
+  }
+
+  const handleValueChange = (e) => {
+    setValue(e.target.value);
+    console.log(value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Calculate the maximum cut with a <span className="theme">quantum computer</span></h1>
       </header>
+      <form>
+        <input type="text" onChange={handleValueChange}></input>
+      </form>
+      <button className="btn theme" onClick={handleClick}>Calculate maxcut!</button>
+
+      <p>{res}</p>
     </div>
   );
 }
